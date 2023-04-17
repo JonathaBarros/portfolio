492b2e900724f3298af462e3f0bdd4f49339fa18
unknown <jonathaalvees@gmail.com>
2023-04-17 02:39:52 -0300

    added the main text and the page title

--- jonatha/pages/_document.tsx
+++ jonatha/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Jonatha | Software Engineer</title>
       <body>
         <Main />
         <NextScript />
--- jonatha/pages/index.tsx
+++ jonatha/pages/index.tsx
@@ -5,29 +5,36 @@ import logo from '../assets/logo.png'
 export default function Home() {
   return (
 
-    <div>
+    <>
       <header className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-          <Image
-            src={logo}
-            alt="Logo"
-            width={80}
-            height={80}
-          />
+        <Image
+          src={logo}
+          alt="Logo"
+          width={80}
+          height={80}
+        />
 
         <nav className='flex flex-row justify-center'>
-          <a className='m-4 '>About me</a>
-          <a className='m-4'>My projects</a>
-          <a className='m-4'>Contact</a>
+          <a href='' className='m-4 '>About me</a>
+          <a href='' className='m-4'>My projects</a>
+          <a href='' className='m-4'>Contact</a>
         </nav>
       </header>
 
 
-      <div  className='max-w-screen-xl flex flex-col items-center justify-between mx-auto p-4' >
-        <main>main</main>
-        <footer>oi</footer>
+      <div className='max-w-screen-xl flex flex-col items-center justify-between mx-auto p-4' >
+        <main>
+          <div className='outline-dotted'>
+          Hi, I'm Jonatha!
+          <h1 className='font-bold '> A software engineer passionate about <br/> building Mobile and Web Apps</h1>
+          </div>
+         
+          <div>
+
+          </div>
+        </main>
+        <footer></footer>
       </div>
-
-
-    </div>
+    </>
   )
 }
