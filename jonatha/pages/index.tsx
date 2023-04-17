8b52bb87aa2b1ee7e4ea0f8a1b3c818f9cc0952e
unknown <jonathaalvees@gmail.com>
2023-04-17 00:50:35 -0300

    fixed layout

--- jonatha/pages/index.tsx
+++ jonatha/pages/index.tsx
@@ -1,7 +1,27 @@
+import Image from 'next/image'
+import logo from '../assets/logo.png'
+
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="">Jonatha</h1>
-    </main>
+
+    <div>
+      <nav className='flex flex-row justify-around mt-2'>
+        <Image
+          src={logo}
+          alt="Logo"
+          width={80}
+          height={80}
+        />
+        <div className='flex flex-row justify-center '>
+          <button className='m-1'>About me</button>
+          <button className='m-1'>My projects</button>
+          <button>Contact</button>
+        </div>
+      </nav>
+
+      <main>main</main>
+      <footer>oi</footer>
+    </div>
   )
 }
