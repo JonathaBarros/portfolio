e69c119bf4d48255e0e9021651d17185a5646cb5
unknown <jonathaalvees@gmail.com>
2023-04-24 21:03:21 -0300

    new version of the design following my figma file

--- jonatha/pages/index.tsx
+++ jonatha/pages/index.tsx
@@ -6,37 +6,20 @@ export default function Home() {
   return (
 
     <>
-      <header className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-        <Image
-          src={logo}
-          alt="Logo"
-          width={80}
-          height={80}
-        />
-
+      <header className='max-w-screen-lg flex flex-wrap items-center justify-between ml-auto mr-auto'>
+        <div className='w-60 bg-black h-20 text-white font-bebas text-3xl flex items-center justify-center pt-4'>JONATHA.DEV</div>
         <nav className='flex flex-row justify-center'>
-          <a href='' className='m-4 '>About me</a>
-          <a href='' className='m-4'>My projects</a>
-          <a href='' className='m-4'>Contact</a>
+          <a href='' className='m-4 pt-4 font-bebas text-3xl hover:underline underline-offset-8'>WHO AM I?</a>
+          <a href='' className='m-4 pt-4 font-bebas text-3xl hover:underline underline-offset-8'>PROJECTS</a>
+          <a href='' className='m-4 pt-4 font-bebas text-3xl hover:underline underline-offset-8'>HIT ME UP</a>
         </nav>
       </header>
 
 
-      <div className='max-w-screen-xl flex flex-col items-center justify-between mx-auto p-4' >
-        <main>
-          
-          <div id="first-main-card">
-            <p className='pb-4'> Hi, I'm Jonatha.</p>
-            <div className=' shadow-[11px_11px_0px_0px_rgba(0,0,0,1)]'>
-              <div className='outline-4 outline-slate-950 outline p-3'>
-                <h1 className='font-bold '> A software engineer passionate about <br /> building Mobile and Web Apps</h1>
-              </div>
-            </div>
-          </div >
-
-          <div id="second-main-card">
-
-          </div>
+      <div className='max-w-screen-lg flex flex-col items-center justify-between mx-auto p-4' >
+        <main className='flex flex-col items-center'>
+           <h1 className='font-bebas text-6xl mt-28'> A software engineer passionate about <br /> building Mobile and Web Applications.</h1>
+           <div className='-rotate-6 mt-6 w-40 h-14 bg-white font-bebas text-3xl flex items-center justify-center border-2 border-solid rounded border-black shadow-[5px_5px_0px_0px_rgba(0,0,0,1)]'>HIRE ME</div>
         </main>
         <footer></footer>
       </div>
