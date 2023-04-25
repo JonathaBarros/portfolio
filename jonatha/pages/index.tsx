1edeabb8b9d7c03254af7bcd2374233a3cd45146
unknown <jonathaalvees@gmail.com>
2023-04-25 01:59:49 -0300

    added some projects, the who am I tag and the footer (layout wise)

--- jonatha/pages/index.tsx
+++ jonatha/pages/index.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
 import arrow from '../assets/arrow.svg'
+import linkedin from '../assets/linkedin.png'
+import github from '../assets/github.png'
+import ig from '../assets/ig.png'
 
 export default function Home() {
   return (
@@ -14,7 +17,7 @@ export default function Home() {
         </nav>
       </header>
 
-      <main className='max-w-screen-lg flex flex-col items-center justify-between mx-auto p-4' >
+      <main className='max-w-screen-lg flex flex-col items-center justify-between mx-auto pt-4' >
 
         <div id="hero-section" className='flex flex-col items-center'>
           <h1 className='font-bebas text-6xl mt-28'> A software engineer passionate about <br /> building Mobile and Web Applications.</h1>
@@ -23,12 +26,37 @@ export default function Home() {
             <div className='-rotate-6 mt-6 w-40 h-14 bg-white font-bebas text-3xl flex items-center justify-center border-2 border-solid rounded border-black shadow-[5px_5px_0px_0px_rgba(0,0,0,1)]'>HIRE ME</div>
           </div>
         </div>
+        
         <div className=' bg-black font-bebas text-3xl text-white mt-10 h-12 min-w-full flex items-center justify-center'>WHAT HAVE I BEEN UP TO LATELY?</div>
+       
+        <div className='min-w-full mt-10 h-96 border-4 border-solid border-black rounded-md'>
+          <div className='bg-blue-600 border-b-4 border-black font-bebas text-3xl text-white pl-10 h-16 flex items-center'>AGENDA-SECOM</div>
+        </div>
 
+        <div className='min-w-full mt-10 h-96 border-4 border-solid border-black rounded-md'>
+          <div className='bg-green-500 border-b-4 border-black font-bebas text-3xl text-white pl-10 h-16 flex items-center'>CARTEIRINHA DO ESTUDANTE</div>
+        </div>
+
+        <div className=' bg-black font-bebas text-3xl text-white mt-10 h-12 min-w-full flex items-center justify-center mb-10'>WHO AM I?</div>
+       
+        <footer className='min-w-full h-48 bg-black flex flex-row justify-between pt-12 pr-28 pl-20' >
+          <div id="first-colum">
+            <div className=' text-white font-bebas text-3xl'>COPYRIGHT 2023</div>
+            <div className=' text-white font-bebas text-base'>ALL RIGHTS RESERVED</div>
+            <div id="socials" className='flex flex-row pt-4 space-x-6'>
+              <Image src={ig} height={38} width={38} alt={'Instagram'}/>
+              <Image src={linkedin} height={38} width={38} alt={'Linkedin'}/>
+              <Image src={github} height={38} width={38} alt={'Github'}/>
+            </div>
+
+          </div>
+
+            <div className=' text-white font-bebas text-3xl'>JONATHA.DEV</div>
+        </footer>
       </main>
 
       
-      <footer></footer>
+
     </>
   )
 }
