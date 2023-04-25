5a8379bdcd19e2a63fd0daa0e9513d19c16ccb86
unknown <jonathaalvees@gmail.com>
2023-04-24 22:35:04 -0300

    finished the layout of the first section

--- jonatha/pages/index.tsx
+++ jonatha/pages/index.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
-import logo from '../assets/logo.png'
-
+import arrow from '../assets/arrow.svg'
 
 export default function Home() {
   return (
@@ -15,14 +14,21 @@ export default function Home() {
         </nav>
       </header>
 
+      <main className='max-w-screen-lg flex flex-col items-center justify-between mx-auto p-4' >
 
-      <div className='max-w-screen-lg flex flex-col items-center justify-between mx-auto p-4' >
-        <main className='flex flex-col items-center'>
-           <h1 className='font-bebas text-6xl mt-28'> A software engineer passionate about <br /> building Mobile and Web Applications.</h1>
-           <div className='-rotate-6 mt-6 w-40 h-14 bg-white font-bebas text-3xl flex items-center justify-center border-2 border-solid rounded border-black shadow-[5px_5px_0px_0px_rgba(0,0,0,1)]'>HIRE ME</div>
-        </main>
-        <footer></footer>
-      </div>
+        <div id="hero-section" className='flex flex-col items-center'>
+          <h1 className='font-bebas text-6xl mt-28'> A software engineer passionate about <br /> building Mobile and Web Applications.</h1>
+          <div className='flex flex-row items-center justify-center'>
+            <Image className='pt-24 pr-6' src={arrow} alt={'arrow'} />
+            <div className='-rotate-6 mt-6 w-40 h-14 bg-white font-bebas text-3xl flex items-center justify-center border-2 border-solid rounded border-black shadow-[5px_5px_0px_0px_rgba(0,0,0,1)]'>HIRE ME</div>
+          </div>
+        </div>
+        <div className=' bg-black font-bebas text-3xl text-white mt-10 h-12 min-w-full flex items-center justify-center'>WHAT HAVE I BEEN UP TO LATELY?</div>
+
+      </main>
+
+      
+      <footer></footer>
     </>
   )
 }
