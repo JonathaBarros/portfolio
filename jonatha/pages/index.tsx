14f3fa5e65cb33e30b62b9bbbf7144eec39a715d
unknown <jonathaalvees@gmail.com>
2023-04-25 11:23:37 -0300

    added some functionality to the layout

--- jonatha/pages/index.tsx
+++ jonatha/pages/index.tsx
@@ -10,11 +10,13 @@ export default function Home() {
 
     <>
       <header className='max-w-screen-lg flex flex-wrap items-center justify-between ml-auto mr-auto'>
-        <div className='w-60 bg-black h-20 text-white font-bebas text-3xl flex items-center justify-center pt-4'>JONATHA.DEV</div>
+        <a href=''>
+          <div className='w-60 bg-black h-20 text-white font-bebas text-3xl flex items-center justify-center pt-4'>JONATHA.DEV</div>
+        </a>
         <nav className='flex flex-row justify-center'>
-          <a href='' className='m-4 pt-4 font-bebas text-3xl hover:underline underline-offset-8'>WHO AM I?</a>
-          <a href='' className='m-4 pt-4 font-bebas text-3xl hover:underline underline-offset-8'>PROJECTS</a>
-          <a href='' className='m-4 pt-4 font-bebas text-3xl hover:underline underline-offset-8'>HIT ME UP</a>
+          <a href='#who-am-i' className='m-4 pt-4 font-bebas text-3xl hover:underline underline-offset-8'>WHO AM I?</a>
+          <a href='#projects' className='m-4 pt-4 font-bebas text-3xl hover:underline underline-offset-8'>PROJECTS</a>
+          <a href='#hit-me-up' className='m-4 pt-4 font-bebas text-3xl hover:underline underline-offset-8'>HIT ME UP</a>
         </nav>
       </header>
 
@@ -27,9 +29,9 @@ export default function Home() {
             <div className='-rotate-6 mt-6 w-40 h-14 bg-white font-bebas text-3xl flex items-center justify-center border-2 border-solid rounded border-black shadow-[5px_5px_0px_0px_rgba(0,0,0,1)]'>HIRE ME</div>
           </div>
         </div>
-        
-        <div className=' bg-black font-bebas text-3xl text-white mt-10 h-12 min-w-full flex items-center justify-center'>WHAT HAVE I BEEN UP TO LATELY?</div>
-       
+
+        <div id="projects" className=' bg-black font-bebas text-3xl text-white mt-10 h-12 min-w-full flex items-center justify-center'>WHAT HAVE I BEEN UP TO LATELY?</div>
+
         <div className='min-w-full mt-10 h-96 border-4 border-solid border-black rounded-md'>
           <div className='bg-blue-600 border-b-4 border-black font-bebas text-3xl text-white pl-10 h-16 flex items-center'>AGENDA-SECOM</div>
         </div>
@@ -38,16 +40,16 @@ export default function Home() {
           <div className='bg-green-500 border-b-4 border-black font-bebas text-3xl text-white pl-10 h-16 flex items-center'>CARTEIRINHA DO ESTUDANTE</div>
         </div>
 
-        <div className=' bg-black font-bebas text-3xl text-white mt-10 h-12 min-w-full flex items-center justify-center mb-10'>WHO AM I?</div>
-    
-        <Image className='rounded-full border-solid border-4 border-black grayscale' src={me} height={128} width={128} alt={'Jonatha'}/>
+        <div id="who-am-i" className=' bg-black font-bebas text-3xl text-white mt-10 h-12 min-w-full flex items-center justify-center mb-10'>WHO AM I?</div>
+
+        <Image className='rounded-full border-solid border-4 border-black grayscale' src={me} height={128} width={128} alt={'Jonatha'} />
         <div className='text-black font-bebas text-3xl mt-2'>JONATHA ALVES</div>
         <div className='min-w-full mt-10 mb-10 h-96 border-4 border-solid border-black rounded-md'>
 
         </div>
 
-        <div className=' bg-black font-bebas text-3xl text-white  h-12 min-w-full flex items-center justify-center mb-10'>HIT ME UP!</div>
-        
+        <div id='hit-me-up' className=' bg-black font-bebas text-3xl text-white  h-12 min-w-full flex items-center justify-center mb-10'>HIT ME UP!</div>
+
         <div className='flex justify-center flex-col items-center border-4 border-solid rounded border-black shadow-[14px_14px_0px_0px_rgba(0,0,0,1)] min-w-[calc(100%-14px)] mr-4 mt-10 mb-20 h-auto pb-10 space-y-8'>
           <div className='text-black font-bebas text-3xl mt-2 flex justify-center pt-4'>LET'S WORK TOGETHER ;)</div>
           <div className='text-black font-bebas text-3xl w-96 border-2 pl-4 border-solid rounded border-black shadow-[5px_5px_0px_0px_rgba(0,0,0,1)]'>NAME</div>
@@ -63,18 +65,24 @@ export default function Home() {
             <div className=' text-white font-bebas text-3xl'>COPYRIGHT 2023</div>
             <div className=' text-white font-bebas text-base'>ALL RIGHTS RESERVED</div>
             <div id="socials" className='flex flex-row pt-4 space-x-6'>
-              <Image src={ig} height={38} width={38} alt={'Instagram'}/>
-              <Image src={linkedin} height={38} width={38} alt={'Linkedin'}/>
-              <Image src={github} height={38} width={38} alt={'Github'}/>
+              <a href='https://www.instagram.com/jonathallves'>
+                <Image src={ig} height={38} width={38} alt={'Instagram'} />
+              </a>
+              <a href='https://www.linkedin.com/in/jonathabarros/'>
+                <Image src={linkedin} height={38} width={38} alt={'Linkedin'} />
+              </a>
+              <a href='https://github.com/JonathaBarros'>
+                <Image src={github} height={38} width={38} alt={'Github'} />
+              </a>
             </div>
 
           </div>
 
-            <div className=' text-white font-bebas text-3xl'>JONATHA.DEV</div>
+          <div className=' text-white font-bebas text-3xl'>JONATHA.DEV</div>
         </footer>
       </main>
 
-      
+
 
     </>
   )
