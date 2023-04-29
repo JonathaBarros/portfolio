80e30d1a771951061fdf99a120db24f2d1de8389
unknown <jonathaalvees@gmail.com>
2023-04-28 22:35:23 -0300

    fixed layout and added photos to the projects

--- jonatha/pages/index.tsx
+++ jonatha/pages/index.tsx
@@ -7,6 +7,10 @@ import ig from '../assets/ig.png'
 import flutter from '../assets/flutter.png'
 import firebase from '../assets/firebase.png'
 import getx from '../assets/getx.png'
+import nextjs from '../assets/nextjs.png'
+import js from '../assets/js.png'
+import agenda from '../assets/agenda.png'
+import carteirinha from '../assets/carteirinha.png'
 import me from '../assets/me.jpg'
 import { useEffect } from 'react'
 
@@ -57,6 +61,7 @@ export default function Home() {
             <div className='max-w-[50%] m-20'>
               <div className='h-96 w-64 border-4 border-black border-solid relative'>
                 <Image className='w-10 h-10 absolute -top-10 left-64' src={camera} alt={'camera icon'} />
+                <Image className='object-scale-down pt-2 max-h-full' src={agenda} alt={"agenda-secom"}></Image>
               </div>
             </div>
 
@@ -72,7 +77,6 @@ export default function Home() {
               </div>
 
             </div>
-
           </div>
         </div>
 
@@ -84,6 +88,7 @@ export default function Home() {
             <div className='max-w-[50%] m-20'>
               <div className='h-96 w-64 border-4 border-black border-solid relative'>
                 <Image className='w-10 h-10 absolute -top-10 left-64' src={camera} alt={'camera icon'} />
+                <Image className='object-fill max-h-full' src={carteirinha} alt={"agenda-secom"}></Image>
               </div>
             </div>
 
@@ -91,10 +96,10 @@ export default function Home() {
             <div className=' text-black font-bebas text-3xl max-w-[50%] mt-10 p-8 flex flex-col items-center justify-center'>
               <span>a web app that is designed for municipal government staff. It simplifies the process of generating identification cards for students who use government buses to get to school</span>
               <h1 className='mt-10'>TECH USED</h1>
-              <div className='flex flex-row items-center justify-center mt-10'>
-                <Image src={flutter} height={38} width={133} alt={"flutter logo"}></Image>
-                <Image src={firebase} width={138} height={61} alt={"firebase logo"}></Image>
-                <Image src={getx} width={114} height={45} alt={'getx logo'}></Image>
+              <div className='flex flex-row items-center justify-center mt-10 space-x-6'>
+                <Image src={js} height={45} width={45} alt={"js logo"}></Image>
+                <Image className='pl-5' src={firebase} width={138} height={61} alt={"firebase logo"}></Image>
+                <Image src={nextjs} width={110} height={42} alt={'nextjs logo'}></Image>
               </div>
 
             </div>
@@ -112,10 +117,11 @@ export default function Home() {
 
         <div className='flex justify-center flex-col items-center border-4 border-solid rounded border-black shadow-[14px_14px_0px_0px_rgba(0,0,0,1)] min-w-[calc(100%-14px)] mr-4 mt-10 mb-20 h-auto pb-10 space-y-8'>
           <div className='text-black font-bebas text-3xl mt-2 flex justify-center pt-4'>LET'S WORK TOGETHER ;)</div>
-          <div className='text-black font-bebas text-3xl w-96 border-2 pl-4 border-solid rounded border-black shadow-[5px_5px_0px_0px_rgba(0,0,0,1)]'>NAME</div>
-          <div className='text-black font-bebas text-3xl w-96 border-2 pl-4 border-solid rounded border-black shadow-[5px_5px_0px_0px_rgba(0,0,0,1)]'>EMAIL</div>
-          <div className='text-black font-bebas text-3xl w-6/12 h-48 p-4 border-2 border-solid rounded border-black shadow-[5px_5px_0px_0px_rgba(0,0,0,1)]'>...I WANT TO HAVE YOU IN MY TEAM!!</div>
-          <div className='mt-6 w-52 h-10 bg-blue-600 font-bebas text-3xl flex items-center justify-center border-2 border-solid rounded border-black shadow-[5px_5px_0px_0px_rgba(0,0,0,1)]'>SEND MESSAGE</div>
+          <input type='text'placeholder='NAME' required className='text-black placeholder-gray-600 font-bebas text-3xl w-96 border-2 pl-4 border-solid rounded border-black shadow-[5px_5px_0px_0px_rgba(0,0,0,1)]'></input>  
+          <input type='text' placeholder='EMAIL' required className='text-black placeholder-gray-600 font-bebas text-3xl w-96 border-2 pl-4 border-solid rounded border-black shadow-[5px_5px_0px_0px_rgba(0,0,0,1)]'></input>
+            <textarea placeholder='...I WANT TO HAVE YOU IN MY TEAM!!' className='text-black placeholder-gray-600 min-w-[50%] font-bebas text-3xl h-48 p-4 border-2 border-solid rounded border-black shadow-[5px_5px_0px_0px_rgba(0,0,0,1)]'>
+            </textarea>
+          <div className='hover:scale-110 transform duration-100 cursor-pointer mt-6 w-52 h-12 bg-blue-600 font-bebas text-3xl flex items-center justify-center border-2 border-solid rounded border-black shadow-[5px_5px_0px_0px_rgba(0,0,0,1)]'>SEND MESSAGE</div>
         </div>
 
         <footer className='min-w-full h-48 bg-black flex flex-row justify-between pt-12 pr-28 pl-20 mt-10' >
@@ -137,6 +143,8 @@ export default function Home() {
           <div id="6" tabIndex={6} onClick={() => scrollToView("home")} className=' text-white font-bebas text-3xl cursor-pointer h-9'>JONATHA.DEV</div>
         </footer>
       </main>
+
+
     </>
   )
 }
